5d51ac371c9171b02561ecd360bdbcc03f1b858b
nirupan <nirupanjoej@gmail.com>
2021-05-27 18:31:37 +0530

    Hide filter Button when there is no todo

--- src/components/filterButton.js
+++ src/components/filterButton.js
@@ -1,11 +1,19 @@
 import { React } from 'react';
 import context from '../core/context';
+import TodoManager from '../services/todoManager';
 
-const FilterButton = (filter) =>
-	<button
-		onClick={ () => context.actions.filterButton(filter) }
-	>
-		{ filter }
-	</button>;
+const FilterButton = (filter) => {
+	const noTodos = TodoManager.getTodoCount(context.state.todos) === 0;
+
+	return noTodos
+		? null
+		: <button
+			// eslint-disable-next-line indent
+				onClick={ () => context.actions.filterButton(filter) }
+		// eslint-disable-next-line no-mixed-spaces-and-tabs
+		  >
+			{ filter }
+		</button>;
+};
 
 export default FilterButton;
