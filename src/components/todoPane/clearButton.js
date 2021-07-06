bc4e133132d09d1bba378fbf9c377de11b77b4ec
nirupan <nirupanjoej@gmail.com>
2021-07-06 16:13:09 +0530

    clearButton by material-ui

--- src/components/todoPane/clearButton.js
+++ src/components/todoPane/clearButton.js
@@ -1,6 +1,8 @@
 import { React } from 'react';
 import context from '../../core/context';
 import TodoManager from '../../services/todoManager';
+import DeleteSweepTwoToneIcon from '@material-ui/icons/DeleteSweepTwoTone';
+import Button from '@material-ui/core/Button';
 
 const ClearButton = () => {
 	const noCompletedTodo = TodoManager.getCompletedTodo(context.state.todos)
@@ -9,12 +11,11 @@ const ClearButton = () => {
 	return noCompletedTodo
 		? null
 		: <span>
-			<button
-				className="btn"
+			<Button
 				onClick={ context.actions.clearButton }
 			>
-				ClearButton
-			</button>
+				<DeleteSweepTwoToneIcon/>
+			</Button>
 		</span>;
 };
 
