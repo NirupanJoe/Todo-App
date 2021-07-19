b8ebaba4d4f748ec900b74d3a39202b8eccde7e3
NirupanJoe <nirupanjoej@gmail.com>
2021-07-19 10:43:00 +0000

    change theme

--- src/components/lightTheme.js
+++ src/components/lightTheme.js
@@ -3,12 +3,20 @@ import { createTheme } from '@material-ui/core';
 const lightTheme = createTheme({
 	palette: {
 		primary: {
-			main: '#f44336',
+			light: '#b71c1c',
+			main: '#1b5e20',
+			dark: '#0d47a1',
 			contrastText: '#fff',
 		},
 		secondary: {
+			light: '#f57f17',
 			main: '#ab47bc',
+			dark: '#212121',
 			contrastText: '#fff',
+		},
+		action: {
+			disabledBackground: 'orange',
+			disabled: 'white',
 		},
 	},
 });
